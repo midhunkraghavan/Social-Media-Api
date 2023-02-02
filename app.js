564d6d0b3bd48f3351e8d1c151a331ba1afaa3cd
midhunkraghavan <midhunkraghavan@gmail.com>
2023-02-02 21:09:56 +0530

    Bug fixed while connecting to database

--- app.js
+++ app.js
@@ -7,24 +7,13 @@ const helemt = require('helmet')
 const cors = require('cors')
 const morgan = require('morgan')
 
+mongoose.set("strictQuery", false)
+mongoose.connect('mongodb://127.0.0.1:27017/socialmedia',
+  { useUnifiedTopology: true, useNewUrlParser: true }, (error) => {
+    if (!error) console.log("Database Conencted Successfully");
+    else console.log(error)
 
-// mongoose.connect('mongodb://127.0.0.1:27017/socialmedia', (error) => {
-//     if (error) {
-//         console.log("err");
-//         console.log(error)
-//     }
-//     else {
-//         console.log("No error");
-//         console.log("Database Conencted Successfully");
-//     }
+  })
 
-// })
-main().catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
-  
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}
 
 app.listen(3000, () => { console.log("Server is running at: 3000") })
