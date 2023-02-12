f0e3c65ac8b227145458fee53787a97bdc039029
midhunkraghavan <midhunkraghavan@gmail.com>
2023-02-12 20:49:28 +0530

    Routes added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+const todoRouter = require('./todo')
+const authenticationRouter = require('./authentication')
+const postRouter = require('./post')
+const userRouter = require('./user')
+
 router.get('/', (req, res) => [
     res.render('index')
 ])
